4d4569a9a11f55eb8b39920a2d13f531e6966f59
zimward <96021122+zimward@users.noreply.github.com>
2024-03-03 00:13:33 +0100

    general: added wayland neccessairy env variables

--- modules/general.nix
+++ modules/general.nix
@@ -13,6 +13,11 @@
 
   environment.sessionVariables = {
     EDITOR="${pkgs.helix}/bin/hx";
+    SDL_VIDEODRIVER="wayland";
+    QT_QPA_PLATFORM="wayland";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION=1;
+    _JAVA_AWT_WM_NONREPARENTING=1;
+    MOZ_ENABLE_WAYLAND=1;
   };
 
   time.timeZone = "Europe/Berlin";
