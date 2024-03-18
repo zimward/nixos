824fa8210f798bcb16be5fa83949cd6376f9f3a6
zimward <96021122+zimward@users.noreply.github.com>
2024-03-18 20:11:59 +0100

    balanced mode on AC

--- modules/poweropt.nix
+++ modules/poweropt.nix
@@ -5,10 +5,10 @@
         CPU_SCALING_GOVERNOR_ON_AC = "schedutil";
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "balanced";
         CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
 
-        PLATFORM_PROFILE_ON_AC = "performance";
+        PLATFORM_PROFILE_ON_AC = "balanced";
         PLATFORM_PROFILE_ON_BAT = "low-power";
 
         CPU_BOOST_ON_AC=1;
