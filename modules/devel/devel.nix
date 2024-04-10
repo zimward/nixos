5a6ece304f45b0947b999d9153e6f49c3884c62b
zimward <96021122+zimward@users.noreply.github.com>
2024-04-10 15:55:01 +0200

    added python libs

--- modules/devel/devel.nix
+++ modules/devel/devel.nix
@@ -1,8 +1,11 @@
-{ pkgs, ... }:
-{
-  environment.systemPackages = with pkgs;[
+{pkgs, ...}: {
+  environment.systemPackages = with pkgs; [
     git
     rustup
     gcc_multi
+    (pkgs.python3.withPackages (python-pkgs: [
+      python-pkgs.numpy
+      python-pkgs.matplotlib
+    ]))
   ];
 }
