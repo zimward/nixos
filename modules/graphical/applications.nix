da1816f49e4d4aabd083ca66dcbc6925e86d361f
zimward <96021122+zimward@users.noreply.github.com>
2024-04-10 15:54:25 +0200

    mpv thunderbird

--- modules/graphical/applications.nix
+++ modules/graphical/applications.nix
@@ -1,12 +1,12 @@
-{ pkgs, ... }:
-{
-  environment.systemPackages = with pkgs;[
+{pkgs, ...}: {
+  environment.systemPackages = with pkgs; [
     librewolf
     libreoffice-qt
     hunspell #auto correction
     hunspellDicts.de_DE
     hunspellDicts.en_US
     keepassxc
-    mpv    
+    mpv
+    thunderbird
   ];
 }
