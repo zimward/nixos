12eb0030671a35a4f91dc6ebe8cf65739e615619
zimward <96021122+zimward@users.noreply.github.com>
2024-05-01 16:54:09 +0200

    removed unused wifi network

--- modules/net/wifi.nix
+++ modules/net/wifi.nix
@@ -45,25 +45,5 @@
         method = "auto";
       };
     };
-    GvR24 = {
-      connection = {
-        id = "Gerald von Riva";
-        type = "wifi";
-      };
-      ipv4.method = "auto";
-      ipv6 = {
-        add-gen-mode = "default";
-        method = "auto";
-      };
-      wifi = {
-        mode = "infrastructure";
-        ssid = "Gerald von Riva 2,4GHz";
-      };
-      wifi-security = {
-        auth-alg = "open";
-        key-mgmt = "wpa-psk";
-        psk = "secret:${config.sops.secrets."wifi/GvR".path}";
-      };
-    };
   };
 }
