6c600917a231f4b3e0410635dcb140ceeb50b70c
zimward <96021122+zimward@users.noreply.github.com>
2024-05-08 18:23:14 +0200

    flake: added unstable as overlay

--- modules/graphical/kicad.nix
+++ modules/graphical/kicad.nix
@@ -6,14 +6,20 @@
 }:
 {
   options = {
-    graphical.kicad.enable = lib.mkOption {
-      default = true;
-      description = "enable kicad and other EE programms";
+    graphical.kicad = {
+      enable = lib.mkOption {
+        default = true;
+        description = "enable kicad and other EE programms";
+      };
+      unstable = lib.mkOption {
+        default = false;
+      };
+      minimal = lib.mkOption {
+        default = false;
+      };
     };
   };
   config = lib.mkIf (config.graphical.enable && config.graphical.kicad.enable) {
-    environment.systemPackages = with pkgs; [
-      kicad
-    ];
+    environment.systemPackages = with pkgs; lib.mkIf (config.graphical.kicad.unstable) [unstable.kicad];
   };
 }
