3903fa4b247363cef32a993c0228b29f85882067
zimward <96021122+zimward@users.noreply.github.com>
2024-05-27 07:31:26 +0200

    modules:use all options of kicad module

--- modules/graphical/kicad.nix
+++ modules/graphical/kicad.nix
@@ -20,6 +20,10 @@
     };
   };
   config = lib.mkIf (config.graphical.enable && config.graphical.kicad.enable) {
-    environment.systemPackages = with pkgs; lib.mkIf (config.graphical.kicad.unstable) [unstable.kicad];
+    environment.systemPackages = with pkgs;
+      lib.mkIf (config.graphical.kicad.unstable && !config.graphical.kicad.minimal) [unstable.kicad]
+      ++ lib.mkIf (config.graphical.kicad.unstable && config.kicad.minimal) [unstable.kicad-small]
+      ++ lib.mkIf (!config.graphical.kicad.unstable && !config.graphical.kicad.minimal) [kicad]
+      ++ lib.mkIf (!config.graphical.kicad.unstable && config.kicad.minimal) [kicad-small];
   };
 }
