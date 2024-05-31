d499150b4475a3ce2c1033a9df662f9bbff71ec3
zimward <96021122+zimward@users.noreply.github.com>
2024-05-31 09:51:52 +0200

    laptop: set kicad to minimal

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -17,57 +17,60 @@ flake-overlays: {
     ../../modules/hardware/poweropt.nix
     ../../modules/net/wifi.nix
   ];
+  config.graphical.kicad.minimal = true;
 
-  nixpkgs.overlays = [] ++ flake-overlays;
+  config = {
+    nixpkgs.overlays = [] ++ flake-overlays;
 
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+    nix.settings.experimental-features = ["nix-command" "flakes"];
 
-  networking.hostName = "laptop"; # Define your hostname.
-  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
-  boot.loader.grub = {
-    enable = true;
-    device = "nodev";
-    enableCryptodisk = true;
-  };
-  #obsidian
-  nixpkgs.config.allowUnfreePredicate = pkg:
-    builtins.elem (lib.getName pkg) [
-      "obsidian"
+    networking.hostName = "laptop"; # Define your hostname.
+    networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
+    boot.loader.grub = {
+      enable = true;
+      device = "nodev";
+      enableCryptodisk = true;
+    };
+    #obsidian
+    nixpkgs.config.allowUnfreePredicate = pkg:
+      builtins.elem (lib.getName pkg) [
+        "obsidian"
+      ];
+    environment.systemPackages = with pkgs; [
+      obsidian
     ];
-  environment.systemPackages = with pkgs; [
-    obsidian
-  ];
-  nixpkgs.config.permittedInsecurePackages = [
-    "electron-25.9.0"
-  ];
+    nixpkgs.config.permittedInsecurePackages = [
+      "electron-25.9.0"
+    ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
+    # Some programs need SUID wrappers, can be configured further or are
+    # started in user sessions.
+    # programs.mtr.enable = true;
+    programs.gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+    };
+
+    # List services that you want to enable:
+
+    # Enable the OpenSSH daemon.
+    services.openssh = {
+      enable = true;
+      settings.PasswordAuthentication = false;
+      settings.KbdInteractiveAuthentication = false;
+    };
+
+    users.users.${config.main-user.userName}.openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIZ4iv3QwB03x5UlteFjPmTymPb29ruuKiMdZLn8jIem mobian@pinephone"
+    ];
+
+    # Open ports in the firewall.
+    # networking.firewall.allowedTCPPorts = [ ... ];
+    # networking.firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    networking.firewall.enable = false;
+
+    # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+    system.stateVersion = "23.11"; # Did you read the comment?
   };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh = {
-    enable = true;
-    settings.PasswordAuthentication = false;
-    settings.KbdInteractiveAuthentication = false;
-  };
-
-  users.users.${config.main-user.userName}.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIZ4iv3QwB03x5UlteFjPmTymPb29ruuKiMdZLn8jIem mobian@pinephone"
-  ];
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  networking.firewall.enable = false;
-
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "23.11"; # Did you read the comment?
 }
