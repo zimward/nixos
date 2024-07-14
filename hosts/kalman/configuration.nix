ed62e65343087e51570afdb8ddd9ebf47e44f63a
zimward <96021122+zimward@users.noreply.github.com>
2024-07-14 09:43:11 +0200

    kalman: delete nix builder tmpfiles after 1 day

--- hosts/kalman/configuration.nix
+++ hosts/kalman/configuration.nix
@@ -17,7 +17,7 @@
       environment.TMPDIR = "/nix/tmp";
     };
     systemd.tmpfiles.rules = [
-      "d /nix/tmp 0755 root root id"
+      "d /nix/tmp 0755 root root 1d"
     ];
     #gets wiped due to tmpfs
     main-user.hashedPassword = "$6$qMlVwZLXPsEw1yMa$DveNYjYb8FO.bJXuNbZIr..Iylt4SXsG3s4Njp2sMVokhEAr0E66WsMm.uNPUXsuW/ankujT19cL6vaesmaN9.";
