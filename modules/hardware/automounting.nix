50efcd611828f74e7fa376386932420c7730fa84
zimward <96021122+zimward@users.noreply.github.com>
2024-07-20 13:55:21 +0200

    modules: automounting: implemented mounting via systemd-mount

--- modules/hardware/automounting.nix
+++ modules/hardware/automounting.nix
@@ -13,5 +13,8 @@
   config = lib.mkIf config.automount.enable {
     services.udisks2.enable = true;
     services.gvfs.enable = true;
+    services.udev.extraRules = ''
+      ACTION=="add", SUBSYSTEMS=="usb", SUBSYSTEM=="block", ENV{ID_FS_USAGE}=="filesystem", RUN{program}+="${pkgs.systemd}/bin/systemd-mount --no-block --automount=yes --collect --owner ${config.main-user.userName} $devnode"
+    '';
   };
 }
