fdbe139f4e4bb8528357dbbc2d1229753fbbde37
zimward <96021122+zimward@users.noreply.github.com>
2024-08-01 09:51:09 +0200

    modules: filter: added enable and extraDomain options

--- modules/net/filter.nix
+++ modules/net/filter.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ lib, config, ... }:
 let
   blacklist = host: ''
     0.0.0.0 ${host}
@@ -7,11 +7,30 @@ let
   concat = lib.lists.foldr (a: b: a + b) "\n";
 in
 {
-  networking.extraHosts = concat (
-    map blacklist [
-      "reddit.com"
-      "old.reddit.com"
-      "www.reddit.com"
-    ]
-  );
+  options = {
+    net.filter = {
+      enable = lib.mkEnableOption ''
+        hostfile based domain blacklist. 
+        Usefull to keep me from doomscrolling (and potenitally block tracking servers)'';
+      extraDomains = lib.mkOption {
+        default = [ ];
+        type = with lib.types; listOf nonEmptyStr;
+        description = "List of additional domains to be blacklisted.";
+      };
+    };
+  };
+  config = lib.mkIf config.net.filter.enable {
+    networking.extraHosts = concat (
+      map blacklist (
+        [
+          "reddit.com"
+          "old.reddit.com"
+          "www.reddit.com"
+          "pornhub.com"
+          "youporn.com"
+        ]
+        ++ config.net.filter.extraDomains
+      )
+    );
+  };
 }
