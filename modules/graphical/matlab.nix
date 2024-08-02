d7b4e9589b1302b9b273db474bfc33c8a311c5f0
zimward <96021122+zimward@users.noreply.github.com>
2024-08-02 15:37:41 +0200

    module: matlab: integrated hm module into main module

--- modules/graphical/matlab.nix
+++ modules/graphical/matlab.nix
@@ -8,6 +8,24 @@
   options = {
     graphical.matlab.enable = lib.mkEnableOption "Matlab";
   };
-  #provided by the matlab overlay
-  config = lib.mkIf config.graphical.matlab.enable { environment.systemPackages = [ pkgs.matlab ]; };
+  config = lib.mkIf config.graphical.matlab.enable {
+    hm.modules = [
+      (
+        { ... }:
+        {
+          #add install dir for matlab
+          home.file = {
+            ".config/matlab/nix.sh" = {
+              executable = true;
+              text = ''
+                INSTALL_DIR=$HOME/.local/share/matlab/
+              '';
+            };
+          };
+        }
+      )
+    ];
+    #provided by the matlab overlay
+    environment.systemPackages = [ pkgs.matlab ];
+  };
 }
