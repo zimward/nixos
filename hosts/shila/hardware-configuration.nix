1d0603df31a6001a6bd66a27bb0392009eb80a4e
zimward <96021122+zimward@users.noreply.github.com>
2024-08-03 23:40:46 +0200

    host: shila: enabled zram swap to potentialy avoid OOM

--- hosts/shila/hardware-configuration.nix
+++ hosts/shila/hardware-configuration.nix
@@ -22,7 +22,7 @@
     fsType = "ext4";
   };
 
-  swapDevices = [ ];
+  zramSwap.enable = true;
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
