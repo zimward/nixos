5683ed685844986cd531048b9a303420bef379eb
zimward <96021122+zimward@users.noreply.github.com>
2024-08-04 17:57:47 +0200

    shila: increase zram percent

--- hosts/shila/hardware-configuration.nix
+++ hosts/shila/hardware-configuration.nix
@@ -22,7 +22,10 @@
     fsType = "ext4";
   };
 
-  zramSwap.enable = true;
+  zramSwap = {
+    enable = true;
+    memoryPercent = 80;
+  };
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
