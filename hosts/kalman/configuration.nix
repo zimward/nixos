8a7761e1c40c6027d75781e2453fcbc14e7fafb8
zimward <96021122+zimward@users.noreply.github.com>
2024-08-17 10:58:37 +0200

    kalman: add specialization for free time

--- hosts/kalman/configuration.nix
+++ hosts/kalman/configuration.nix
@@ -7,18 +7,10 @@
   ];
 
   config = {
-    systemd.services.nix-daemon = {
-      environment.TMPDIR = "/nix/tmp";
-    };
-    systemd.tmpfiles.rules = [ "d /nix/tmp 0755 root root 1d" ];
     #gets wiped due to tmpfs
     main-user.hashedPassword = "$6$qMlVwZLXPsEw1yMa$DveNYjYb8FO.bJXuNbZIr..Iylt4SXsG3s4Njp2sMVokhEAr0E66WsMm.uNPUXsuW/ankujT19cL6vaesmaN9.";
 
     ethernet.share.device = "enp49s0f3u3";
-
-    graphical.steam.enable = true;
-    graphical.deluge.enable = true;
-    graphical.ime.enable = true;
 
     boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen;
     boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
@@ -29,7 +21,7 @@
     #workaround
     services.logrotate.checkConfig = false;
 
-    networking.hostName = "kalman"; # Define your hostname.
+    networking.hostName = "kalman";
 
     environment.systemPackages = with pkgs; [ docker ];
 
@@ -53,8 +45,6 @@
     ];
 
     hardware.opentabletdriver.enable = true;
-    graphical.minecraft.enable = true;
-    graphical.ereader.enable = true;
 
     programs.gnupg.agent = {
       enable = true;
@@ -62,6 +52,14 @@
     };
     # since no services are supposed to run on this machine a firewall would only wase memory
     networking.firewall.enable = false;
+
+    specialisation.freizeit.configuration = {
+      graphical.steam.enable = true;
+      graphical.deluge.enable = true;
+      graphical.ime.enable = true;
+      graphical.minecraft.enable = true;
+      graphical.ereader.enable = true;
+    };
 
     # nix.buildMachines = [
     #   {
