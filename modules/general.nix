740539fbd3e9cef93119287b7bac26f6a8abb50d
zimward <96021122+zimward@users.noreply.github.com>
2024-08-31 01:11:29 +0200

    sway: refactored config and removed unessesairy components which in some parts caused unwanted behaviour

--- modules/general.nix
+++ modules/general.nix
@@ -18,13 +18,7 @@
       zellij.enable = true;
     };
     environment.sessionVariables = {
-      SDL_VIDEODRIVER = "wayland";
-      QT_QPA_PLATFORM = "wayland";
-      QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-      _JAVA_AWT_WM_NONREPARENTING = "1";
-      MOZ_ENABLE_WAYLAND = "1";
       SSH_AUTH_SOCK = "/run/user/1000/ssh-agent";
-      PATH = "$HOME/.local/bin/";
     };
     net.filter.enable = true;
     #running ssh agent on graphical hosts is most often needed
--- modules/graphical/sway.nix
+++ modules/graphical/sway.nix
@@ -4,34 +4,6 @@
   lib,
   ...
 }:
-let
-  #propagate sway env vars
-  dbus-sway-environment = pkgs.writeTextFile {
-    name = "dbus-sway-environment";
-    destination = "/bin/dbus-sway-environment";
-    executable = true;
-
-    text = ''
-      dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
-      systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
-      systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
-    '';
-  };
-  configure-gtk = pkgs.writeTextFile {
-    name = "configure-gtk";
-    destination = "/bin/configure-gtk";
-    executable = true;
-    text =
-      let
-        schema = pkgs.gsettings-desktop-schemas;
-        datadir = "${schema}/share/gsettings-schemas/${schema.name}";
-      in
-      ''
-        gnome_schema=org.gnome.desktop.interface
-        gsettings set $gnome_schema gtk-theme 'palenight'
-      '';
-  };
-in
 {
   imports = [ ./sway_cfg.nix ];
   options = {
@@ -40,42 +12,38 @@ in
   config = lib.mkIf (config.graphical.enable && config.graphical.sway.enable) {
     environment.systemPackages = with pkgs; [
       alacritty
-      dbus
-      dbus-sway-environment
-      configure-gtk
       xdg-utils
-      glib
-      palenight-theme
       adwaita-icon-theme
       grim # screenshot
       slurp # screenshot
       wl-clipboard
       bemenu
-      mako
+      mako # wayland notification service
     ];
-    services.dbus.enable = true;
+    services.dbus = {
+      enable = true;
+      apparmor = "enabled";
+    };
     xdg.portal = {
       enable = true;
       wlr.enable = true;
-      extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+      extraPortals = [ pkgs.xdg-desktop-portal-wlr ];
     };
-
-    # hardware.opengl = {
-    #   enable = true;
-    #   driSupport = true;
-    # };
 
     programs.sway = {
       enable = true;
-      wrapperFeatures.gtk = true;
-
-      extraSessionCommands = ''
-        export SDL_VIDEODRIVER=wayland
-        export QT_QPA_PLATFORM=wayland
-        export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
-        export _JAVA_AWT_WM_NONREPARENTING=1
-        export MOZ_ENABLE_WAYLAND=1
-      '';
+      extraSessionCommands = lib.strings.concatLines (
+        [
+          ''
+            export SDL_VIDEODRIVER=wayland
+            export QT_QPA_PLATFORM=wayland
+            export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
+            export _JAVA_AWT_WM_NONREPARENTING=1
+            export MOZ_ENABLE_WAYLAND=1
+          ''
+        ]
+        ++ lib.optionals config.graphical.ime.enable [ "fcitx5 -d" ]
+      );
     };
 
     services.xserver.xkb.layout = "de";
