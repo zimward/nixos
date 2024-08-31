311c0c3822bd1632c9d1dea11078bccc95be07f5
zimward <96021122+zimward@users.noreply.github.com>
2024-08-31 17:51:10 +0200

    nushell: login-workaround: start sway on login

---
+++ modules/cli/nushell/login-workaround.nix
@@ -0,0 +1,26 @@
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+{
+  #only use this on graphical to work around fcitx5
+  config = lib.mkIf (config.cli.nushell.enable && config.graphical.enable) {
+    users.users."${config.main-user.userName}".shell = pkgs.bash;
+    #start nushell if run inside alacritty
+    programs.bash = {
+      loginShellInit = ''
+        if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
+          exec sway
+        fi
+      '';
+      interactiveShellInit = ''
+        if [[ $TERM == "alacritty" ]]
+        then
+          nu
+        fi
+      '';
+    };
+  };
+}
