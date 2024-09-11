b58adcf34e1dfa3d3402d7b8abf9e3d1232585ec
zimward <96021122+zimward@users.noreply.github.com>
2024-09-11 10:28:30 +0900

    orsted: change tz to tokyo

--- hosts/orsted/configuration.nix
+++ hosts/orsted/configuration.nix
@@ -8,6 +8,16 @@
   ];
 
   config = {
+    time.timeZone = "Asia/Tokyo";
+    virtualisation.libvirtd = {
+      enable = true;
+      #user mode networking
+      allowedBridges = [ "virbr0" ];
+    };
+
+    virtualisation.spiceUSBRedirection.enable = true;
+    programs.virt-manager.enable = true;
+
     graphical.kicad.minimal = true;
 
     networking.hostName = "orsted"; # Define your hostname.
@@ -28,9 +38,15 @@
       settings.KbdInteractiveAuthentication = false;
     };
 
-    users.users.${config.main-user.userName}.openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIZ4iv3QwB03x5UlteFjPmTymPb29ruuKiMdZLn8jIem mobian@pinephone"
-    ];
+    users.users.${config.main-user.userName} = {
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIZ4iv3QwB03x5UlteFjPmTymPb29ruuKiMdZLn8jIem mobian@pinephone"
+      ];
+      extraGroups = [
+        "libvirtd"
+        "dialout"
+      ];
+    };
     # only ssh is running with pubkey auth so a firewall would only waste memory
     networking.firewall.enable = false;
     graphical.ime.enable = true;
