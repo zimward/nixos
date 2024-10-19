be67132a852401a1f1d40bdace3fbc5dd2aa2de3
zimward <96021122+zimward@users.noreply.github.com>
2024-10-20 00:14:31 +0900

    base: add doas-sudo-shim to allow --use-remote-sudo

--- modules/cli/update.nix
+++ modules/cli/update.nix
@@ -6,9 +6,8 @@ let
   updScript = pkgs.writeShellScriptBin "update.sh" ''
     cd /tmp
     git clone --depth 1 shilagit:git/nixos
-    doas chown root:root -R ./nixos/
-    doas nixos-rebuild switch --flake ./nixos/.
-    doas rm -rf /tmp/nixos
+    nixos-rebuild switch --flake ./nixos/. --use-remote-sudo
+    rm -rf /tmp/nixos
   '';
 in
 {
