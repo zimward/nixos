365f3367521a83e277c0919e423e276ff620d981
zimward <96021122+zimward@users.noreply.github.com>
2025-03-09 10:31:13 +0100

    aisha: matrix: enable coturn for audio calls; make needed state persistent

--- hosts/aisha/matrix.nix
+++ hosts/aisha/matrix.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, ... }:
 let
   port = 8448;
   fqdn = "zimward.moe";
@@ -10,17 +10,22 @@ let
     add_header Access-Control-Allow-Origin *;
     return 200 '${builtins.toJSON data}';
   '';
+  certDir = config.security.acme.certs.${fqdn}.directory;
 in
 {
-  services.postgresql.enable = true;
+  services.postgresql = {
+    enable = true;
+    dataDir = "/nix/persist/system/postgresql/";
+  };
   services.matrix-synapse = {
     enable = true;
+    dataDir = "/nix/persist/system/matrix-synapse/";
     settings = {
       registration_shared_secret = "Y2q16w4KrsXxwsE1QvBBMb4a716I3qhaZxzUR6uVcNdVOmKhCLoDCTngkMJk99Ot";
       server_name = "zimward.moe";
       public_baseurl = "https://zimward.moe";
-      tls_certificate_path = "/var/lib/acme/zimward.moe/fullchain.pem";
-      tls_private_key_path = "/var/lib/acme/zimward.moe/key.pem";
+      tls_certificate_path = "${certDir}/fullchain.pem";
+      tls_private_key_path = "${certDir}/key.pem";
       extraConfig = ''
         max_upload_size: "100M"
       '';
@@ -62,6 +67,13 @@ in
           ];
         }
       ];
+      turn_uris = [
+        "turn:${fqdn}:3478?transport=udp"
+        "turn:${fqdn}:3478?transport=tcp"
+      ];
+      turn_shared_secret = config.services.coturn.static-auth-secret;
+      turn_user_lifetime = "1h";
+      turn_allow_guests = true;
     };
   };
   #auto-discovery via .well-known
@@ -74,12 +86,69 @@ in
   #restart matrix after cert change
   security.acme.certs = {
     "zimward.moe" = {
-      postRun = "systemctl restart matrix-synapse.service";
+      postRun = "systemctl restart matrix-synapse.service; systemctl restart coturn.service;";
     };
   };
   #allow synapse to read the ssl cert
   users.users."matrix-synapse".extraGroups = [ "nginx" ];
 
-  #federation port
-  networking.firewall.allowedTCPPorts = [ port ];
+  #coturn for audio calls
+  services.coturn = {
+    enable = true;
+    no-cli = true;
+    no-tcp-relay = true;
+    min-port = 49000;
+    max-port = 50000;
+    use-auth-secret = true;
+    static-auth-secret = "YS8MDZEFOe4BF6N8DVsPVUmQWfdCe4XxVIbJVKzGbn3zssZdoc1tGTlfDZ575VeU";
+    realm = fqdn;
+    cert = "${certDir}/full.pem";
+    pkey = "${certDir}/key.pem";
+    extraConfig = ''
+      # ban private IP ranges
+      no-multicast-peers
+      denied-peer-ip=0.0.0.0-0.255.255.255
+      denied-peer-ip=10.0.0.0-10.255.255.255
+      denied-peer-ip=100.64.0.0-100.127.255.255
+      denied-peer-ip=127.0.0.0-127.255.255.255
+      denied-peer-ip=169.254.0.0-169.254.255.255
+      denied-peer-ip=172.16.0.0-172.31.255.255
+      denied-peer-ip=192.0.0.0-192.0.0.255
+      denied-peer-ip=192.0.2.0-192.0.2.255
+      denied-peer-ip=192.88.99.0-192.88.99.255
+      denied-peer-ip=192.168.0.0-192.168.255.255
+      denied-peer-ip=198.18.0.0-198.19.255.255
+      denied-peer-ip=198.51.100.0-198.51.100.255
+      denied-peer-ip=203.0.113.0-203.0.113.255
+      denied-peer-ip=240.0.0.0-255.255.255.255
+      denied-peer-ip=::1
+      denied-peer-ip=64:ff9b::-64:ff9b::ffff:ffff
+      denied-peer-ip=::ffff:0.0.0.0-::ffff:255.255.255.255
+      denied-peer-ip=100::-100::ffff:ffff:ffff:ffff
+      denied-peer-ip=2001::-2001:1ff:ffff:ffff:ffff:ffff:ffff:ffff
+      denied-peer-ip=2002::-2002:ffff:ffff:ffff:ffff:ffff:ffff:ffff
+      denied-peer-ip=fc00::-fdff:ffff:ffff:ffff:ffff:ffff:ffff:ffff
+      denied-peer-ip=fe80::-febf:ffff:ffff:ffff:ffff:ffff:ffff:ffff
+    '';
+  };
+
+  users.users."turnserver".extraGroups = [ "nginx" ];
+
+  networking.firewall.allowedUDPPortRanges = [
+    {
+      from = config.services.coturn.min-port;
+      to = config.services.coturn.max-port;
+    }
+  ];
+
+  #federation port + coturn
+  networking.firewall.allowedTCPPorts = [
+    port
+    3478
+    5349
+  ];
+  networking.firewall.allowedUDPPorts = [
+    3478
+    5349
+  ];
 }
