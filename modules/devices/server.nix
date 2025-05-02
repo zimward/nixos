7a55a7ead6d492cfd27e6be3d28d0e2ccdbb4856
zimward <zimward@zimward.moe>
2025-05-02 17:24:38 +0200

    devices/server: upgrade allow reboot

--- modules/devices/server.nix
+++ modules/devices/server.nix
@@ -3,5 +3,7 @@
   config = lib.mkIf (config.device.class == "server") {
     motd.enable = true;
     documentation.enable = false;
+    system.autoUpgrade.allowReboot = true;
+    system.autoUpgrade.dates = lib.mkForce "4:00";
   };
 }
