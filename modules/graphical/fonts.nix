eb451c69d28e92d476e5e71161685b39bdbeb9db
zimward <zimward@zimward.moe>
2025-05-18 12:44:12 +0200

    fonts: fix conditional enablement

--- modules/graphical/fonts.nix
+++ modules/graphical/fonts.nix
@@ -1,16 +1,19 @@
-{ pkgs, config, ... }:
 {
-  fonts = {
-    packages =
-      with pkgs;
-      lib.optionals config.graphical.enable [
-        noto-fonts
-        noto-fonts-cjk-sans
-        liberation_ttf
-        fira-code
-        fira-code-symbols
-        nerd-fonts.fira-code
-      ];
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+{
+  fonts = lib.mkIf config.graphical.enable {
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk-sans
+      liberation_ttf
+      fira-code
+      fira-code-symbols
+      nerd-fonts.fira-code
+    ];
     fontDir.enable = true;
   };
 
