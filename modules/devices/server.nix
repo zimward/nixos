bd47fadb708f0d4be53b94dd9b5b6ff90037a5e9
zimward <zimward@zimward.moe>
2025-05-18 11:46:10 +0200

    servers: disable xdg stuff

--- modules/devices/server.nix
+++ modules/devices/server.nix
@@ -3,6 +3,12 @@
   config = lib.mkIf (config.device.class == "server") {
     motd.enable = true;
     documentation.enable = false;
+    xdg = {
+      autostart.enable = false;
+      icons.enable = false;
+      menus.enable = false;
+      sounds.enable = false;
+    };
     system.autoUpgrade.allowReboot = true;
     system.autoUpgrade.dates = lib.mkForce "4:00";
   };
