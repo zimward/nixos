7e65d5e7aec7bb60254e85f78cc5ad8e0ade91ab
zimward <zimward@zimward.moe>
2025-05-18 12:51:13 +0200

    nushell: login workaround dont depend on graphical options

--- modules/cli/nushell/login-workaround.nix
+++ modules/cli/nushell/login-workaround.nix
@@ -11,20 +11,15 @@
     default = null;
   };
   #only use this on graphical to work around fcitx5
-  config =
-    lib.mkIf
-      (
-        config.cli.nushell.enable && config.graphical.enable && config.cli.nushell.graphical_startup != null
-      )
-      {
-        users.users."${config.main-user.userName}".shell = lib.mkForce pkgs.bash;
-        #start nushell if run inside alacritty
-        programs.bash = {
-          loginShellInit = ''
-            if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
-              exec ${config.cli.nushell.graphical_startup}
-            fi
-          '';
-        };
-      };
+  config = lib.mkIf (config.cli.nushell.enable && config.cli.nushell.graphical_startup != null) {
+    users.users."${config.main-user.userName}".shell = lib.mkForce pkgs.bash;
+    #start nushell if run inside alacritty
+    programs.bash = {
+      loginShellInit = ''
+        if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
+          exec ${config.cli.nushell.graphical_startup}
+        fi
+      '';
+    };
+  };
 }
