3dfbd2639b5a3971d8a09e4c68ee08f1eb70545b
zimward <zimward@zimward.moe>
2025-05-18 12:30:37 +0200

    desktop: move some options into graphical

--- modules/devices/desktop.nix
+++ modules/devices/desktop.nix
@@ -6,18 +6,10 @@
 }:
 {
   config = lib.mkIf (config.device.class == "desktop") {
-    latex = true;
-    devel = {
-      helix.enable = true;
-      git.enable = true;
-      zellij.enable = true;
-    };
     environment.sessionVariables = {
       SSH_AUTH_SOCK = "/run/user/1000/ssh-agent";
     };
     net.filter.enable = true;
-    #running ssh agent on graphical hosts is most often needed
-    cli.ssh.enableAgent = true;
     #sound
     sys.sound.enable = true;
     #fix for opening links
