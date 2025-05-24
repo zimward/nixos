56018755f134eef00584160e89659dafe19af3f7
zimward <zimward@zimward.moe>
2025-05-24 13:11:28 +0200

    hddIdle: use globbed udev rules

--- modules/hardware/hddIdle.nix
+++ modules/hardware/hddIdle.nix
@@ -6,29 +6,16 @@
 }:
 {
   options = {
-    hardware.hddIdle = lib.mkOption {
-      description = "List of HDDs to put into idle mode";
-      type = lib.types.nullOr (lib.types.listOf lib.types.str);
-      default = null;
-    };
-    hardware.hddIdleTime = lib.mkOption {
+    hardware.hddIdle.enable = lib.mkEnableOption "hdd idle rules";
+    hardware.hddIdle.time = lib.mkOption {
       description = "Time for hdd to go into idle. In multiple of 5s intervals";
       type = lib.types.int;
       default = 200;
     };
   };
-  config = lib.mkIf (config.hardware.hddIdle != null) {
-    systemd.services.hdd-idle = {
-      wantedBy = [ "multi-user.target" ];
-      unitConfig = {
-        StopWhenUnneeded = "yes";
-      };
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart = map (
-          hdd: "${lib.getExe pkgs.hdparm} -S ${builtins.toString config.hardware.hddIdleTime} /dev/${hdd}"
-        ) config.hardware.hddIdle;
-      };
-    };
+  config = lib.mkIf config.hardware.hddIdle.enable {
+    services.udev.extraRules = ''
+      ACTION=="add|change", KERNEL=="sd[a-z]", ATTRS{queue/rotational}=="1", RUN+="${lib.getExe pkgs.hdparm} -B 127 -S ${builtins.toString config.hardware.hddIdle.time} /dev/%k"
+    '';
   };
 }
