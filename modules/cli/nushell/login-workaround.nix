56c111e57fb58708c115e44956dc6f7ba8c07b79
zimward <zimward@zimward.moe>
2025-06-09 14:29:02 +0200

    nushell: remove outdated comments

--- modules/cli/nushell/login-workaround.nix
+++ modules/cli/nushell/login-workaround.nix
@@ -10,10 +10,8 @@
     description = "graphical startup command";
     default = null;
   };
-  #only use this on graphical to work around fcitx5
   config = lib.mkIf (config.cli.nushell.enable && config.cli.nushell.graphical_startup != null) {
     users.users."${config.main-user.userName}".shell = lib.mkForce pkgs.bash;
-    #start nushell if run inside alacritty
     programs.bash = {
       loginShellInit = ''
         if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
