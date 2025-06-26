9fa909aba2c5ea83d30b9e451c235640974585ca
zimward <zimward@zimward.moe>
2025-06-26 16:36:44 +0200

    steam: remove unneeded workaround

--- modules/graphical/steam.nix
+++ modules/graphical/steam.nix
@@ -1,7 +1,6 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }:
 {
@@ -21,21 +20,6 @@
       "steam-run"
     ];
     programs.steam.enable = true;
-    programs.steam.package = pkgs.steam.override {
-      extraPkgs =
-        pkgs: with pkgs; [
-          xorg.libXcursor
-          xorg.libXi
-          xorg.libXinerama
-          xorg.libXScrnSaver
-          libpng
-          libpulseaudio
-          libvorbis
-          stdenv.cc.cc.lib
-          libkrb5
-          keyutils
-        ];
-    };
     hardware.steam-hardware.enable = true;
     programs.gamescope.enable = true;
   };
