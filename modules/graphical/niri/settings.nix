5d3c24b784f64b782c2cdacc0be253f91165a0ed
zimward <zimward@zimward.moe>
2025-07-13 15:38:10 +0200

    niri: don't set display manually

--- modules/graphical/niri/settings.nix
+++ modules/graphical/niri/settings.nix
@@ -14,7 +14,6 @@
     hm.programs.niri.settings = {
       environment = {
         CLUTTER_BACKEND = "wayland";
-        DISPLAY = ":0";
         GDK_BACKEND = "wayland,x11";
         MOZ_ENABLE_WAYLAND = "1";
         NIXOS_OZONE_WL = "1";
