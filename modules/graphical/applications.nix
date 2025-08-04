4882bd40156836086fdd3478bab78b3b419cc0e2
zimward <zimward@zimward.moe>
2025-08-04 12:58:43 +0200

    applications: remove bookworm due to being insecure and unused

--- modules/graphical/applications.nix
+++ modules/graphical/applications.nix
@@ -15,7 +15,6 @@ in
       default = config.device.class == "desktop";
       description = "enable default graphical applications";
     };
-    ereader.enable = lib.mkEnableOption "E-Book reader";
     minecraft.enable = lib.mkEnableOption "minecraft launcher";
     deluge.enable = lib.mkEnableOption "Deluge torrent client";
     obsidian.enable = lib.mkEnableOption "Obsidian";
@@ -23,8 +22,7 @@ in
   };
   config = lib.mkIf (config.graphical.default.applications.enable && config.graphical.enable) {
     environment.systemPackages =
-      enPkg cfg.ereader pkgs.bookworm
-      ++ enPkg cfg.minecraft pkgs.prismlauncher
+      enPkg cfg.minecraft pkgs.prismlauncher
       ++ enPkg cfg.deluge pkgs.deluge
       ++ enPkg cfg.obsidian pkgs.obsidian
       ++ enPkg cfg.irc pkgs.weechat
