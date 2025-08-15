90e155473640db1cd445e0e43a87cd86f2807363
zimward <zimward@zimward.moe>
2025-08-15 13:24:10 +0200

    kalman: have openwebui running

--- hosts/kalman/configuration.nix
+++ hosts/kalman/configuration.nix
@@ -66,6 +66,15 @@
     };
     graphical.ime.enable = true;
 
+    services.open-webui = {
+      enable = true;
+      environment = {
+        OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
+        WEBUI_AUTH = "False";
+      };
+    };
+
+    nixpkgs.allowUnfreePackages = [ "open-webui" ];
     environment.systemPackages = [
       (pkgs.writeShellScriptBin "ollama" ''
         HSA_OVERRIDE_GFX_VERSION=10.3.0 ${lib.getExe pkgs.ollama-rocm} $@
