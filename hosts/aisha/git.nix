c4e27c87d82eec944104524f82ab7e36e442ea97
zimward <zimward@zimward.moe>
2025-10-01 00:37:59 +0200

    aisha/git: add calops to git

--- hosts/aisha/git.nix
+++ hosts/aisha/git.nix
@@ -32,6 +32,7 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHD1nysJ0LC+tg4r02bCaIRJ+3ZsJEh+91X7irPhtbn0 root@orsted"
       "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIPdS0w5zf2JcGuSv4Q6MqvjnREh6FrwtGB0wMEehIwx6AAAABHNzaDo=" # nhnn
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILmnknd6bSmWrhpr+I5j3R5fou8gu8zY4V3oc+gTfVuH" # kirottu
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHnhIy51JVnJ5GlkWNqb0yAFPAfnipGXo+CUnc822BKR" # calops
     ];
   };
   systemd.timers.updateFlake = {
