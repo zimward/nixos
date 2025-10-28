14aa9f3428e5a18c9943971d5329c03e1e168895
zimward <zimward@zimward.moe>
2025-10-28 21:33:38 +0100

    kalman: unused deprecated config

--- hosts/kalman/config.nix
+++ hosts/kalman/config.nix
@@ -92,34 +92,34 @@
       pkgs.sbctl
     ];
 
-    hm.programs.helix.package = pkgs.symlinkJoin {
-      name = "helix";
-      paths = [ pkgs.helix ];
-      buildInputs = [ pkgs.makeWrapper ];
-      postBuild = ''
-        wrapProgram $out/bin/hx \
-          --set HANDLER "ollama" \
-          --set OLLAMA_MODEL "gemma3n:e2b"
-      '';
-    };
-    hm.programs.helix.languages = {
-      language-server.gpt = {
-        command = lib.getExe pkgs.helix-gpt;
-      };
-      language = [
-        {
-          name = "matlab";
-          language-servers = [ "gpt" ];
-        }
-        {
-          name = "rust";
-          language-servers = [
-            "rust-analyzer"
-            "gpt"
-          ];
-        }
-      ];
-    };
+    # hm.programs.helix.package = pkgs.symlinkJoin {
+    #   name = "helix";
+    #   paths = [ pkgs.helix ];
+    #   buildInputs = [ pkgs.makeWrapper ];
+    #   postBuild = ''
+    #     wrapProgram $out/bin/hx \
+    #       --set HANDLER "ollama" \
+    #       --set OLLAMA_MODEL "gemma3n:e2b"
+    #   '';
+    # };
+    # hm.programs.helix.languages = {
+    #   language-server.gpt = {
+    #     command = lib.getExe pkgs.helix-gpt;
+    #   };
+    #   language = [
+    #     {
+    #       name = "matlab";
+    #       language-servers = [ "gpt" ];
+    #     }
+    #     {
+    #       name = "rust";
+    #       language-servers = [
+    #         "rust-analyzer"
+    #         "gpt"
+    #       ];
+    #     }
+    #   ];
+    # };
 
     systemd.network.networks."10-lan" = {
       matchConfig.Name = "enp35s0f*";
