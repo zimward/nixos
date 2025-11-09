2d2835b1fe9daddf9a73053b2ace31d52f056691
zimward <zimward@zimward.moe>
2025-11-09 11:25:20 +0100

    doga/minecraft: use declarative config & add whitelist

--- hosts/doga/minecraft.nix
+++ hosts/doga/minecraft.nix
@@ -6,10 +6,16 @@
     eula = true;
     openFirewall = true;
     jvmOpts = "-Xmx4092M -XX:+UseG1GC";
+    declarative = true;
     serverProperties = {
       difficulty = 3;
-      gamemode = 1;
+      gamemode = 0;
       motd = "NixOS Minecraft server!";
+    };
+    whitelist = {
+      user1 = "050ad88c-78da-4c2a-8b30-f6ca9f45c129";
+      user2 = "527daba0-faa2-4256-8dcb-8a7f803f8e87";
+      user3 = "510f1ed3-3103-41a6-af5a-65c06577fed7";
     };
   };
 }
