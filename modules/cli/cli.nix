ac8c06398a4c8ee2cd40dcd35e66dd1f9b10d6b5
zimward <zimward@zimward.moe>
2025-11-25 20:59:04 +0100

    cli: remove home-manager refs

--- modules/cli/cli.nix
+++ modules/cli/cli.nix
@@ -12,26 +12,28 @@
     };
   };
   config = lib.mkIf config.cli.enable {
-    environment.systemPackages = with pkgs; [
-      nushell
-      starship
-      ripgrep
-      bottom # process manager
-      dust
-      sshfs
-      fd
-      fzf
-    ];
-    #rarely needed on servers
-    hm.programs.yazi.enable = config.graphical.enable;
-    hm.programs.yazi.package = (
-      pkgs.yazi.override {
-        optionalDeps = with pkgs; [
-          ripgrep
-          fd
-          fzf
+    environment.systemPackages =
+      with pkgs;
+      [
+        nushell
+        starship
+        ripgrep
+        bottom # process manager
+        dust
+        sshfs
+        fd
+        fzf
+      ]
+      ++
+        #rarely needed on servers
+        lib.optionals config.graphical.enable [
+          (pkgs.yazi.override {
+            optionalDeps = with pkgs; [
+              ripgrep
+              fd
+              fzf
+            ];
+          })
         ];
-      }
-    );
   };
 }
