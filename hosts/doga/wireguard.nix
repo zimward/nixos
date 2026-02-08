f7170126b80589c87abf304d85aaddc3e5b96131
zimward <zimward@zimward.moe>
2026-02-08 10:31:05 +0100

    doga: wireguard: set permanent routing rules for wg0

--- hosts/doga/wireguard.nix
+++ hosts/doga/wireguard.nix
@@ -1,3 +1,7 @@
+{ pkgs, ... }:
+let
+  table = 1337;
+in
 {
   systemd.network.networks."50-wg" = {
     matchConfig.Name = "wg0";
@@ -7,7 +11,7 @@
     ];
     routingPolicyRules = [
       {
-        Table = 1337;
+        Table = table;
         User = "minecraft";
         Priority = 30000;
         Family = "ipv6";
@@ -29,10 +33,14 @@
         AllowedIPs = [ "::/0" ];
         Endpoint = "[2a01:4f9:c012:36f5::1]:51820";
         PersistentKeepalive = 25;
-        RouteTable = 1337;
+        RouteTable = table;
       }
     ];
   };
+  networking.firewall.extraCommands = ''
+    ${pkgs.iproute2}/bin/ip -6 rule add iif wg0 lookup ${toString table}
+    ${pkgs.iproute2}/bin/ip -6 rule add oif wg0 lookup ${toString table}
+  '';
 
   environment.persistence."/nix/persist/system" = {
     directories = [ "/etc/credstore" ];
