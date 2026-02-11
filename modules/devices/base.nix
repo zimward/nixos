657377c4acc321e74d3d2d03cdb0646f62b0711f
zimward <zimward@zimward.moe>
2026-02-11 22:48:46 +0100

    base: optimize zram performance

--- modules/devices/base.nix
+++ modules/devices/base.nix
@@ -48,6 +48,13 @@
 
     #doesn't hurt
     zramSwap.enable = true;
+    boot.kernel.sysctl = {
+      # https://wiki.archlinux.org/title/Zram#Optimizing_swap_on_zram
+      "vm.swappiness" = 180;
+      "vm.watermark_boost_factor" = 0;
+      "vm.watermark_scale_factor" = 125;
+      "vm.page_cluster" = 0;
+    };
 
     time.timeZone = lib.mkDefault "Europe/Berlin";
     i18n.defaultLocale = "de_DE.UTF-8";
