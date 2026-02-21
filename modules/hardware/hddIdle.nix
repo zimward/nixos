c5488ec1298e0f933c7dfbbc390ef8728a5ed173
zimward <zimward@zimward.moe>
2026-02-21 23:01:35 +0100

    doga: redo hdd idle

--- modules/hardware/hddIdle.nix
+++ modules/hardware/hddIdle.nix
@@ -10,12 +10,12 @@
     hardware.hddIdle.time = lib.mkOption {
       description = "Time for hdd to go into idle. In multiple of 5s intervals";
       type = lib.types.int;
-      default = 500;
+      default = 200;
     };
   };
   config = lib.mkIf config.hardware.hddIdle.enable {
     services.udev.extraRules = ''
-      ACTION=="add|change", KERNEL=="sd[a-z]", ATTRS{queue/rotational}=="1", RUN+="${lib.getExe pkgs.hdparm} -B 128 -S ${toString config.hardware.hddIdle.time} /dev/%k"
+      ACTION=="add|change", KERNEL=="sd[a-z]", ATTRS{queue/rotational}=="1", RUN+="${lib.getExe pkgs.hdparm} -B 127 -S ${toString config.hardware.hddIdle.time} /dev/%k"
     '';
   };
 }
