4b70da536807b502fd0921f370d66680f76795d0
zimward <zimward@zimward.moe>
2026-02-24 09:52:15 +0100

    doga: minecraft niri-offtopic

--- hosts/doga/minecraft.nix
+++ hosts/doga/minecraft.nix
@@ -19,4 +19,47 @@
       user3 = "510f1ed3-3103-41a6-af5a-65c06577fed7";
     };
   };
+  containers = {
+    niriot = {
+      autoStart = true;
+      ephemeral = true;
+      bindMounts = {
+        "/minecraft-niri" = {
+          hostPath = "/nix/persist/minecraft-niri";
+          isReadOnly = false;
+        };
+      };
+
+      config =
+        { modulesPath, lib, ... }:
+        {
+          imports = [ "${modulesPath}/profiles/minimal.nix" ];
+          config = {
+            nixpkgs.config.allowUnfreePredicate =
+              pkg:
+              builtins.elem (lib.getName pkg) [
+                "minecraft-server"
+              ];
+            services.minecraft-server = {
+              enable = true;
+              dataDir = "/minecraft-niri";
+              eula = true;
+              openFirewall = true;
+              jvmOpts = "-Xmx4092M -XX:+UseG1GC";
+              declarative = true;
+              serverProperties = {
+                server-port = 25566;
+                difficulty = 3;
+                gamemode = 0;
+                motd = "NixOS Minecraft server for niri-offtopic";
+              };
+            };
+            networking.firewall.enable = false;
+            system.stateVersion = "26.05";
+          };
+        };
+    };
+  };
+  networking.firewall.allowedTCPPorts = [ 25566 ];
+  networking.firewall.allowedUDPPorts = [ 25566 ];
 }
