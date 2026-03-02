3f53decf55f0ccb70b81c5efdf66033fca4210f6
zimward <zimward@zimward.moe>
2026-03-02 09:34:49 +0100

    matrix: enable livekit

--- hosts/aisha/matrix.nix
+++ hosts/aisha/matrix.nix
@@ -24,14 +24,23 @@ let
   port = 8448;
   fqdn = "matrix.zimward.moe";
   url = "https://${fqdn}";
-  clientConfig."m.homeserver".base_url = url;
-  serverConfig."m.server" = "${fqdn}:${builtins.toString port}";
+  clientConfig = {
+    "m.homeserver".base_url = url;
+    "org.matrix.msc4143.rtc_foci" = [
+      {
+        "type" = "livekit";
+        "livekit_service_url" = "https://${fqdn}/livekit/jwt";
+      }
+    ];
+  };
+  serverConfig."m.server" = "${fqdn}:${toString port}";
   mkWellKnown = data: ''
     default_type application/json;
     add_header Access-Control-Allow-Origin *;
     return 200 '${builtins.toJSON data}';
   '';
   certDir = config.security.acme.certs."zimward.moe".directory; # has a sub cert for fqdn
+  livekitKeyFile = "/run/livekit.key";
 in
 {
   services.postgresql = {
@@ -184,12 +193,60 @@ in
     locations."/_matrix" = proxyConf;
     locations."/_synapse/client" = proxyConf;
 
+    locations."^~ /livekit/sfu/" = {
+      extraConfig = ''
+        proxy_send_timeout 120;
+        proxy_read_timeout 120;
+        proxy_buffering off;
+
+        proxy_set_header Accept-Encoding gzip;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "upgrade";
+      '';
+
+      priority = 400;
+      proxyPass = "http://[::1]:${toString config.services.livekit.settings.port}/";
+      proxyWebsockets = true;
+    };
+
     extraConfig = ''
       ${compressionConf}
       access_log /var/log/nginx/matrix_access.log;
       error_log /var/log/nginx/matrix_error.log;
     '';
   };
+  services.livekit = {
+    enable = true;
+    openFirewall = true;
+    settings.room.auto_create = false;
+    keyFile = livekitKeyFile;
+  };
+  # generate the key when needed
+  systemd.services.livekit-key = {
+    before = [
+      "lk-jwt-service.service"
+      "livekit.service"
+    ];
+    wantedBy = [ "multi-user.target" ];
+    path = with pkgs; [
+      livekit
+      coreutils
+      gawk
+    ];
+    script = ''
+      echo "Key missing, generating key"
+      echo "lk-jwt-service: $(livekit-server generate-keys | tail -1 | awk '{print $3}')" > "${livekitKeyFile}"
+    '';
+    serviceConfig.Type = "oneshot";
+    unitConfig.ConditionPathExists = "!${livekitKeyFile}";
+  };
+  services.lk-jwt-service = {
+    enable = true;
+    # can be on the same virtualHost as synapse
+    livekitUrl = "wss://${fqdn}/livekit/sfu";
+    keyFile = livekitKeyFile;
+  };
+
   #restart matrix after cert change
   security.acme.certs = {
     "zimward.moe" = {
