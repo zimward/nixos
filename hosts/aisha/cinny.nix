2fc174631dee013dde0240619a9b31cb004b67e3
zimward <zimward@zimward.moe>
2026-03-02 08:23:10 +0100

    Revert "aisha: switch cinny to sable"
    This reverts commit f35b5561491abb521df4446418802c39615fa270.

--- hosts/aisha/cinny.nix
+++ hosts/aisha/cinny.nix
@@ -9,59 +9,6 @@ let
     zstd_min_length 256;
     zstd_types  "text/plain" "text/html" "application/json" "application/xml" "application/wasm";
   '';
-
-  sable = pkgs.callPackage (
-    {
-      lib,
-      buildNpmPackage,
-      fetchFromGitHub,
-      giflib,
-      python3,
-      pkg-config,
-      pixman,
-      nodejs_24,
-      cairo,
-      pango,
-      stdenv,
-    }:
-
-    buildNpmPackage (finalAttrs: {
-      pname = "sable";
-      version = "1.0.0-dev";
-
-      src = fetchFromGitHub {
-        owner = "7w1";
-        repo = "sable";
-        rev = "90dd4c721a71cd1365372481ef250b8b39c7ec39";
-        hash = "sha256-4JTz7yIvINQWKv2VXU2WyO/s6Urihv1Fg/cO/GOH73U=";
-      };
-
-      nodejs = nodejs_24;
-
-      npmDepsHash = "sha256-ElKLaNWFJJivo3cby9ALfCwWC+fNE3mF5jgpJ+/9KEc=";
-
-      nativeBuildInputs = [
-        python3
-        pkg-config
-      ];
-
-      buildInputs = [
-        pixman
-        cairo
-        pango
-      ]
-      ++ lib.optionals stdenv.hostPlatform.isDarwin [ giflib ];
-
-      installPhase = ''
-        runHook preInstall
-
-        cp -r dist $out
-
-        runHook postInstall
-      '';
-    })
-  ) { };
-
 in
 {
   services.nginx.virtualHosts."cinny.zimward.moe" = {
@@ -69,7 +16,7 @@ in
     enableACME = true;
     quic = true;
     locations."/" = {
-      root = toString sable;
+      root = toString pkgs.cinny;
       extraConfig = ''
         etag on;
         rewrite ^/config.json$ /config.json break;
