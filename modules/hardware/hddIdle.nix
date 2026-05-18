db9b9153a1f33d375609c4657484083010721446
zimward <zimward@zimward.moe>
2026-05-18 09:56:11 +0200

    hddIdle: fix spindown time

--- modules/hardware/hddIdle.nix
+++ modules/hardware/hddIdle.nix
@@ -8,14 +8,14 @@
   options = {
     hardware.hddIdle.enable = lib.mkEnableOption "hdd idle rules";
     hardware.hddIdle.time = lib.mkOption {
-      description = "Time for hdd to go into idle. In multiple of 5s intervals";
+      description = "Time for hdd to go into idle. In multiple of 5s intervals (if bigger than 240 it becomes 30min intervals)";
       type = lib.types.int;
-      default = 200;
+      default = 241;
     };
   };
   config = lib.mkIf config.hardware.hddIdle.enable {
     services.udev.extraRules = ''
-      ACTION=="add|change", KERNEL=="sd[a-z]", ATTRS{queue/rotational}=="1", RUN+="${lib.getExe pkgs.hdparm} -B 1 -S ${toString config.hardware.hddIdle.time} /dev/%k"
+      ACTION=="add|change", KERNEL=="sd[a-z]", ATTRS{queue/rotational}=="1", RUN+="${lib.getExe pkgs.hdparm} -B 127 -S ${toString config.hardware.hddIdle.time} /dev/%k"
     '';
   };
 }
