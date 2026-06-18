bff36b9fcc869f62dac0350847cc07ae459dff7d
zimward <zimward@zimward.moe>
2026-06-18 10:02:31 +0200

    desktop: remove removed options

--- modules/devices/desktop.nix
+++ modules/devices/desktop.nix
@@ -34,13 +34,8 @@
 
       #sound
       sys.sound.enable = true;
-      #fix for opening links
-      systemd.user.extraConfig = ''
-        DefaultEnvironment="PATH=/run/wrappers/bin:/etc/profiles/per-user/%u/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
-      '';
       environment.systemPackages = [ pkgs.nh ];
       boot.initrd.systemd.network.wait-online.enable = false;
-      boot.bootspec.enable = true;
       boot.kernelPackages = pkgs.linuxPackages_latest;
       systemd.network.wait-online.enable = false;
 
