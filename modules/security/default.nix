4b9da51098408d527bc94cfa0cdbe8912fbef02e
zimward <zimward@zimward.moe>
2026-06-19 17:59:14 +0200

    security: test polkit rule for remote sessions

--- modules/security/default.nix
+++ modules/security/default.nix
@@ -12,6 +12,15 @@
       enable = true;
       adminIdentities = [ "unix-user:${config.mainUser.userName}" ];
       persistentAuthentication = true;
+      extraConfig = ''
+        polkit.addRule(function(action, subject) {
+          if (action.id == "org.freedesktop.systemd1.manage-units" && subject.active ${
+            lib.optionalString (config.device.class == "desktop") "&& subject.local"
+          }) {
+            return polkit.Result.AUTH_ADMIN_KEEP;
+          }
+        });
+      '';
     };
     security.run0-sudo-shim.enable = true;
     security.run0-sudo-shim.package = pkgs.run0-sudo-shim;
